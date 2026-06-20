9d43992e57e8f2abf2c313c27092adc988f30eda
benishbatoolkmk-beep <benishbatoolkmk@gmail.com>
2026-06-20 17:33:51 +0500

    assignment

--- Benish_python_fundamentals01.py
+++ Benish_python_fundamentals01.py
@@ -13,3 +13,31 @@ print(my_dict)
 # set
 my_set = {1, 2, 3, 4, 5}
 print(my_set)
+#conditions
+x = 10
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is less than or equal to 5")
+# loops
+for i in range(5):
+    print(i)
+while x > 0:
+    print(x)
+    x -= 1
+# functions
+def greet(name):
+    return f"Hello, {name}!"
+print(greet("Alice"))
+#Git commands
+# git init
+Current folder ko Git repository bana deta hai.
+# git add .
+“Git, ye files next save (commit) me include karna.”
+# git commit -m "Initial commit"
+“Git, ye files ko save karna with a message describing the changes.”
+
+# git remote add origin <repository_url>
+“Git, ye remote repository ko origin naam se add karna.”
+# git push -u origin master
+“Git, local master branch ko remote repository ke master branch pe push karna aur origin ko track karna.”
